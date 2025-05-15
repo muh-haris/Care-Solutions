ac2b39e0004d3b3883a7dd275d97eafaa03a3351
Muhammad Haris Yasir <115215277+muh-haris@users.noreply.github.com>
2025-05-16 02:47:16 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -21,31 +21,7 @@ export default function Home() {
     </main>
   );
 }
-//------------------
 
-// "use client";
-
-// import Navbar from "./components/Navbar";
-// import Hero from "./components/Hero";
-// import DownloadSection from "./components/DownloadSection";
-// import Introduction from "./components/Introduction";
-// import AppScreenshots from "./components/AppScreenshots";
-// import Contact from "./components/Contact";
-// import Footer from "./components/Footer";
-
-// export default function Home() {
-//   return (
-//     <>
-//       <Navbar />
-//       <main className="pt-40 p-6 max-w-7xl mx-auto bg-white text-gray-800">
-//         <Hero />
-//         <DownloadSection />
-//         <Introduction />
-//         <AppScreenshots />
-//         <Contact />
-//         <Footer />
-//       </main>
-//
-//     </>
-//   );
-// }
+export const metadata = {
+  title: "Care Solutions",
+};
